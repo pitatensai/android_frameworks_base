84f13c8658f2daef6042a2542e6a224b3dd69e89
Todd Kennedy <toddke@google.com>
2020-09-25 08:39:57 -0700

    Handling removing <original-package> tag
    Fixes: 169414761
    Test: Manual
    Test: Add package A
    Test: Verify package A exists
    Test: Remove A, add package B w/ original-package A
    Test: Verify package A exists, but codepath is to package B
    Test: Replace package B w/o original-package A
    Test: Verify package A removed and package B added
    Change-Id: Ib0dbd18fa2f44c2a6b64cc3731d891c44d8d90ab

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -11190,6 +11190,8 @@ public class PackageManagerService extends IPackageManager.Stub
                 mSettings.addRenamedPackageLPw(parsedPackage.getRealPackage(),
                         originalPkgSetting.name);
                 mTransferredPackages.add(originalPkgSetting.name);
+            } else {
+                mSettings.removeRenamedPackageLPw(parsedPackage.getPackageName());
             }
         }
         if (pkgSetting.sharedUser != null) {
